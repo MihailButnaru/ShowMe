1f06cb25af3a8381d6e2e13e8ad3b6cb0f9a7967
MIHAIL BUTNARU <butnarum@hotmail.com>
2017-06-16 12:10:44 +0100

    Convert
    ConvertCreate

--- TravelSoft/src/org/travel/car/CarController.java
+++ TravelSoft/src/org/travel/car/CarController.java
@@ -2,7 +2,11 @@ package org.travel.car;
 
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import static java.lang.Double.parseDouble;
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -40,13 +44,14 @@ public class CarController implements Initializable {
     @FXML private Label costID;
     @FXML private Pane mapID;
     @FXML private RadioButton KimID;
-    @FXML private ChoiceBox engineID;
-    ObservableList<Double> choice = FXCollections.observableArrayList(1.0, 1.5, 1.6, 1.7, 1.8, 2.0,2.2, 2.3, 2.5, 2.6, 2.7, 2.8, 3.0, 3.2, 3.3, 3.5, 3.7, 3.8, 3.9, 4.1, 4.2, 4.4, 4.8, 5.0);
+    private ChoiceBox engineID;
+//    ObservableList<Double> choice = FXCollections.observableArrayList(1.0, 1.5, 1.6, 1.7, 1.8, 2.0,2.2, 2.3, 2.5, 2.6, 2.7, 2.8, 3.0, 3.2, 3.3, 3.5, 3.7, 3.8, 3.9, 4.1, 4.2, 4.4, 4.8, 5.0);
+    @FXML private TextField mpgID;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       engineID.setItems(choice);
-       engineID.getSelectionModel().select(2.2);
+//       engineID.setItems(choice);
+//       engineID.getSelectionModel().select(2.2);
         
     } 
     
@@ -80,14 +85,24 @@ public class CarController implements Initializable {
             //Getting the distance and split in several strings
             String distance = carAp.displayData(startName,endName);
             String[] distanceNumber = distance.split(" ");
+            
             labelID.setText(distanceNumber[0]);
             
             //Getting the travelTime
             String timeTravel = carAp.displayTime(startName, endName);
             timeID.setText(timeTravel);
             
-            //Choice box data
-            double data = (double) engineID.getValue();
+            //MPG Convert To Litres based on the Kilometres 100km
+            String convert =  mpgID.getText();
+            int convertLitres = parseInt(convert);
+            Converter conv = new Converter();
+            double litres = conv.convertMPGToLitres(convertLitres);
+            
+            //Calculating the km / litres to know litres total
+            String calc = distanceNumber[0];
+            double calc1 = Double.parseDouble(calc);
+            double answerCalculation = calc1 / litres;
+            System.out.println(answerCalculation);
             
             
        }
@@ -112,15 +127,10 @@ public class CarController implements Initializable {
        
        
        
-       
-       //Array Engine Size
-       
+             
       
     }
 
-    @FXML
-    private void engineID1(ActionEvent event) {
-    }
 
    
     
